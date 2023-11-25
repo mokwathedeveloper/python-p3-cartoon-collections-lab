7d59b7a5ff3407036a227bb56d5e7bd72e4420f2
mokwathedeveloper <moffatmokwa12@gmail.com>
2023-11-25 21:48:16 +0300

    completed assigment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start=1):
+        print(f"{i}. {dwarf}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + '!' for call in planeteer_calls]
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
     pass
 
-def find_the_cheese():
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in snacks:
+        if item in cheeses:
+            return item
+    return None
     pass
